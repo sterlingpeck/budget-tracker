11ed6a15789796f6c2e2163ce1609c16e5c52409
Sterling Peck <peck.sterling@gmail.com>
2022-08-06 20:57:07 -0600

    transaction upload

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,7 +9,7 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
   dataBase = event.target.result;
   if (navigator.onLine) {
-    uploadRecords();
+    uploadInfo();
   }
 };
 
@@ -18,3 +18,36 @@ function saveInfo(record) {
   const recordObjectStore = transaction.objectStore("new_record");
   recordObjectStore.add(record);
 }
+
+function uploadInfo() {
+  const transaction = dataBase.transaction(["new_record"], "readwrite");
+  const recordObjectStore = transaction.objectStore("new_record");
+  const getAll = recordObjectStore.getAll();
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_record"], "readwrite");
+          const recordObjectStore = transaction.objectStore("new_record");
+          recordObjectStore.clear();
+          alert("All transactions have been logged.");
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadInfo);
